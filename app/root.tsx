21fc2cafb7f3583541b41b88a427853859de5822
coggiee <zentechie7@gmail.com>
2026-01-26 15:30:03 +0900

    chore: 폰트 변경

--- app/root.tsx
+++ app/root.tsx
@@ -33,6 +33,10 @@ export const links: Route.LinksFunction = () => [
 		rel: "stylesheet",
 		href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
 	},
+	{
+		rel: "stylesheet",
+		href: "https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.9/dist/web/variable/pretendardvariable-dynamic-subset.min.css",
+	},
 ];
 
 export const middleware: Route.MiddlewareFunction[] = [clerkMiddleware()];
